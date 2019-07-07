16e3250fb435bd9693cfd5f58aaedbd6779b33bb
chandra4g <chandra4gcg@gmail.com>
2019-07-07 11:06:31 +0530

    first commit

---
+++ src/main/java/maven_demo/App.java
@@ -0,0 +1,7 @@
+package maven_demo;
+
+public class App {
+	public String msg(){
+		return "Hello welcome to new world";
+	}
+}
---
+++ src/test/java/maven_demo/AppTest.java
@@ -0,0 +1,14 @@
+package maven_demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
